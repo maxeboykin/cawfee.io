89f017df3ac0d5440a47e679d586c6ad439ba6d0
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-22 12:39:39 -0500

    errorhandlingendware

--- server/server.js
+++ server/server.js
@@ -16,3 +16,25 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '--', 'public/index
 
 app.use(express.static(path.join(__dirname, '--', 'public')));
 
+//any remaining requests with an extension (.js, .css, etc) send 404
+
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+  } else {
+    next()
+  }
+})
+
+
+//sends index.html
+
+
+//error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internet server errror');
+});
