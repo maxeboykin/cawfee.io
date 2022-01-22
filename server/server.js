4c0cd3327170b09e674c2f08e4f2436bad268569
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-22 01:33:51 -0500

    bodyparsing

--- server/server.js
+++ server/server.js
@@ -4,4 +4,15 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
+
+//body parsing middleware
+app.use(express.json());
+
+//auth and api routes
+app.use('/auth', require('./auth'));
+app.use('/api', require('./api'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '--', 'public/index.html')));
+
 app.use(express.static(path.join(__dirname, '--', 'public')));
+
