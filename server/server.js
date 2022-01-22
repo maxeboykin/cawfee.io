eebb78eda102bf46588ced529bae50d896569700
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-22 01:30:52 -0500

    expressstatic

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-
 const morgan = require('morgan');
+
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '--', 'public')));
