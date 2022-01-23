91e41b71ff4865756b4d6817735ecf8f602f3ac5
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-23 10:16:33 -0500

    ordersAPI

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,12 +1,29 @@
 const router = require('express').Router();
+const { models: { Order }} = require('../db');
+const OrderCoffee = require('../db/models/OrderCoffee');
+module.exports = router;
 
-router.get('/', function (req, res, next) {
-
+router.get('/', async (req, res, next) => {
+try {
+  const orders = await OrderCoffee.findAll()
+  res.json(orders);
+} catch(error) {
+  next(error)
+}
 })
 
 
-router.get('/:orderId', function (req, res, next) {
-
+router.get('/:orderId', async (req, res, next) => {
+try {
+  const order = await OrderCoffee.findAll({
+    where: {
+      orderId: req.params.orderId
+    }
+  })
+  res.json(order);
+} catch(err) {
+  next(err);
+}
 })
 
 
