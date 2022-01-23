acd360838a1e8beb58bc1417fb246e235998f1a0
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-22 22:33:47 -0500

    usersjsstore

--- client/store/users.js
+++ client/store/users.js
@@ -8,7 +8,7 @@ const UPDATE_USER = 'UPDATE_USER';
 
 //action creators
 
-export const setUsers = (users) => ({
+export const _setUsers = (users) => ({
   type: SET_USERS,
   users
 });
@@ -30,3 +30,66 @@ export const _updateUser = (user) => ({
 
 //async creators
 
+export const fetchUsers = () => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem('token');
+    const {data: users} = await Axios.get('/api/users', {
+      headers: {
+        authorization: token
+      }
+    });
+    dispatch(_setUsers(users));
+  }
+}
+
+export const createUser = (user, history) => {
+  return async (dispatch) => {
+    const {data: created} = await Axios.post('/api/users', user);
+    dispatch(_createUser(created));
+    history.push('/');
+  }
+}
+
+export const removeUser = (userId, history) => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem('token');
+
+    const {data: removed} = await Axios.delete(`/api/users/${userId}`, {
+      headers: {
+        authorization: token
+      }
+    });
+    dispatch(_removeUser(removed));
+    history.push('/');
+  }
+}
+
+export const updateUser = (user, history) => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem('token');
+    const {data: updated} = await Axios.put(`api/users/${user.id}`, {
+      user,
+      headers: {
+        authorization: token
+      }
+    });
+    dispatch(_updateUser(updated));
+    history.push('/');
+  }
+}
+
+//reducer
+export default function (state = [], action) {
+  switch(action.type) {
+    case SET_USERS:
+      return action.users;
+    case CREATE_USER:
+      return [...state, action.user];
+    case REMOVE_USER:
+      return [...state.filter(user => user.id !== action.user.id)];
+    case UPDATE_USER:
+      return state.map(user => user.id === action.user.id ? action.user : user);
+    default:
+      return state;
+  }
+}
