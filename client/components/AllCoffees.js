b1851909b22807dd55e6d3aee9ec60ad9dbc056f
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-23 23:30:39 -0500

    allcoffees

---
+++ client/components/AllCoffees.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState } from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import { fetchCoffees } from '../store/plants'
+import { fetchPlant } from '../store/singlePlant';
+import { Link } from "react-router-dom";
+import { me } from '../store/auth';
+import ls from 'local-storage';
+import { addItem, fetchCart } from '../store/cart';
+import { addLocalItem } from '../store/LocalCart';
+
+const AllCoffees = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCoffees());
+    dispatch(me());
+  }, []);
+
+  return (
+
+  );
+}
+
+export default AllCoffees;
