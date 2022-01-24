c9a877622b27400fe9c5f2f95e35d696779d38db
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-24 15:21:53 -0500

    login

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -56,3 +56,49 @@ return (
 }
 
 
+export const Signup = () => {
+  const error = useSelector(state => state.auth.error);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const username = event.target.username.value.toLowerCase();
+    const password = event.target.password.value;
+    const email = event.target.email.value;
+    dispatch(authenticate(username, password, 'signup', email));
+    if (username && password && email) history.push("/coffee");
+  };
+
+return (
+  <div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">
+            Username:
+          </label>
+          <input name="username" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            Password:
+          </label>
+          <input name="password" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email:
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+        {error & error.response && <div>{error.response.data}</div>}
+      </form>
+    </div>
+  </div>
+)
+}
