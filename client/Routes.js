964c447868e1d2ac7bc741770d1ef6117dc7fb28
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-23 22:14:09 -0500

    componentfolders

--- client/Routes.js
+++ client/Routes.js
@@ -1,5 +1,5 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
+import React, {Component, Fragment, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
 import AllCoffees from './components/AllCoffees';
@@ -13,3 +13,54 @@ import CheckoutSummary from './components/CheckoutSummary';
 import ls from 'local-storage';
 
 
+const Routes = () => {
+  const isLoggedIn = useSelector(state => !! state.auth.id);
+  const isAdmin = useSelector(state => state.auth.isAdmin)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(me());
+  }, []);
+
+  return (
+    <div className="main-container">
+    {isLoggedIn ? (
+      isAdmin ? (
+        //this is for admins only
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route exact path = "/coffee" component={AllCoffees} />
+          <Route exact path="/coffee/:coffeeid" component={SingleCoffee} />
+          <Route exact path='/users' component={AllUsers} />
+          <Route exact path='/users/:userId' component={SingleUser} />
+          {/* <Route exact path='/cart' component={CartView} />
+          <Route exact path='/cart/checkout' component={CheckoutSummary} /> */}
+            <Redirect to="/home" />
+          </Switch>
+      ) :
+      //for users not admins
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route exact path = "/coffee" component={AllCoffees} />
+        <Route exact path="/coffee/:coffeeid" component={SingleCoffee} />
+        {/* <Route exact path='/cart' component={CartView} />
+        <Route exact path='/cart/checkout' component={CheckoutSummary} /> */}
+        <Redirect to="/home" />
+      </Switch>
+    ) : (
+      <Switch>
+        <Route path='/' exact component={AllCoffees} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route exact path = "/coffee" component={AllCoffees} />
+        <Route exact path="/coffee/:coffeeId" component={SingleCoffee} />
+        {/* <Route exact path='/cart' component={CartView} />
+        <Route exact path='/cart/checkout' component={CheckoutSummary} /> */}
+            <Redirect to='/coffee' />
+      </Switch>
+    )}
+    </div>
+  )
+}
+
+export default withRouter(Routes);
