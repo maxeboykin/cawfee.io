71be2f42c9425585851112acf7acfef7f65dc4e1
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-25 16:14:01 -0500

    singleCoffeepart1

--- client/components/SingleCoffee.js
+++ client/components/SingleCoffee.js
@@ -1,26 +1,22 @@
 import React, {useEffect, useState } from 'react'
 import { useDispatch, useSelector } from "react-redux"
 import { connect } from 'react-redux'
+import { fetchCoffee, clearCoffee } from '../store/singleCoffee';
+import { me } from '../store/auth';
+
+
 
 const SingleCoffee = () => {
   const dispatch = useDispatch();
   const username = useSelector(state => state.auth.username);
   const userId = useSelector(state => state.auth.id);
+  const targetCoffee = useSelector(state => state.coffee);
+  //const cart = useSelector(state = state.cart);
+
+
 
   return (
-    <div>
-      <main>
-        <div className='WelcomeContainer'>
-          <div className="WelcomeTitleContaienr">
-            <div className='WelcomeTitle'>
-               <h1>Welcome {username} - {userId} </h1>
-               </div>
-               <button>Edit My Account</button>
-               <button>View Previous Orders</button>
-          </div>
-        </div>
-      </main>
-    </div>
+
   )
 
 }
--- public/bundle.js
+++ public/bundle.js
@@ -2622,33 +2622,9 @@ const Navbar = () => {
 /*!*******************************************!*\
   !*** ./client/components/SingleCoffee.js ***!
   \*******************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-
-
-
-
-const SingleCoffee = () => {
-  const dispatch = (0,react_redux__WEBPACK_IMPORTED_MODULE_1__.useDispatch)();
-  const username = (0,react_redux__WEBPACK_IMPORTED_MODULE_1__.useSelector)(state => state.auth.username);
-  const userId = (0,react_redux__WEBPACK_IMPORTED_MODULE_1__.useSelector)(state => state.auth.id);
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-    className: "WelcomeContainer"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-    className: "WelcomeTitleContaienr"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-    className: "WelcomeTitle"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Welcome ", username, " - ", userId, " ")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", null, "Edit My Account"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", null, "View Previous Orders")))));
-};
-
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SingleCoffee);
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/maxwellboykin/Personal Projects/cawfee.io/client/components/SingleCoffee.js: Unexpected token (20:2)\n\n\u001b[0m \u001b[90m 18 |\u001b[39m   \u001b[36mreturn\u001b[39m (\u001b[0m\n\u001b[0m \u001b[90m 19 |\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 20 |\u001b[39m   )\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m   \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 21 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 22 |\u001b[39m }\u001b[0m\n\u001b[0m \u001b[90m 23 |\u001b[39m\u001b[0m\n    at Object._raise (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:506:17)\n    at Object.raiseWithData (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:499:17)\n    at Object.raise (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:460:17)\n    at Object.unexpected (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:3699:16)\n    at Object.parseParenAndDistinguishExpression (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:12760:12)\n    at Object.parseExprAtom (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:12321:23)\n    at Object.parseExprAtom (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:7704:20)\n    at Object.parseExprSubscripts (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:11999:23)\n    at Object.parseUpdate (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:11979:21)\n    at Object.parseMaybeUnary (/Users/maxwellboykin/Personal Projects/cawfee.io/node_modules/@babel/parser/lib/index.js:11954:23)");
 
 /***/ }),
 
