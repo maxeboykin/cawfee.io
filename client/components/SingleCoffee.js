a14aef91cc94467eb98259b802073f00b8b35d49
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-25 16:31:30 -0500

    componentdidunmounthook

--- client/components/SingleCoffee.js
+++ client/components/SingleCoffee.js
@@ -11,9 +11,26 @@ const SingleCoffee = () => {
   const username = useSelector(state => state.auth.username);
   const userId = useSelector(state => state.auth.id);
   const targetCoffee = useSelector(state => state.coffee);
+
+  const [qty, setQty] = useState(1);
+  const [mainImage, setMainImage] = useState("");
+  // const [targetCoffee, setTargetCoffee] = useState({});
+
   //const cart = useSelector(state = state.cart);
 
+  //componentDidMount
+  useEffect(() => {
+    dispatch(fetchCoffee(props.match.params.coffeeId))
+  },[])
 
+//componentDidUnmount
+  useEffect(() => {
+    return async () => {
+      dispatch(clearCoffee());
+      setQty(1);
+      setMainImage("");
+    }
+  }, targetCoffee)
 
   return (
 
